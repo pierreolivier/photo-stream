2a3411a9842943cf7ab555270a9a1211ef8c7977
Pierre-Olivier Bouteau <bouteau.pierreolivier@gmail.com>
2024-10-02 00:05:42 +0200

    main: navigation + introduction view + empty collection

--- PhotoStream/ViewController.swift
+++ PhotoStream/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var introductionView: UIView!
+    
+    @IBOutlet weak var streamCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("Loading app...")
     }
 
-
+    @IBAction func onIntroductionDismiss(_ sender: Any) {
+        print("Dismiss introduction...")
+        
+        UIView.animate(withDuration: 0.4, delay: 0, options: .transitionCrossDissolve, animations: {
+            self.introductionView.alpha = 0
+            
+            // Apply layout updates
+            self.view.layoutIfNeeded()
+        })
+        
+        UIView.animate(withDuration: 0.3, delay: 0.2, options: .transitionCrossDissolve, animations: {
+            self.streamCollectionView.frame.origin.y = self.introductionView.frame.origin.y
+            self.streamCollectionView.frame.size.height += self.introductionView.frame.height
+            
+            // Apply layout updates
+            self.view.layoutIfNeeded()
+        })
+    }
+    
 }
 
