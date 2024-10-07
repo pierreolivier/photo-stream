3f39e8ce3d38e80f77ee7425ed40d97e400ad5d3
Pierre-Olivier Bouteau <bouteau.pierreolivier@gmail.com>
2024-10-07 11:58:31 +0200

    collection service: create service
    - generate an author name from dummy api
    - store photo connection in service
    - use service in photo collection view model

--- PhotoStream/Views/DetailsViewController.swift
+++ PhotoStream/Views/DetailsViewController.swift
@@ -13,6 +13,7 @@ class DetailsViewController: UIViewController {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var author: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +27,8 @@ class DetailsViewController: UIViewController {
             dateFormatter.dateFormat = "MM-dd-yyyy 'at' HH:mm"
             let formattedDate = dateFormatter.string(from: photo.date)
             date.text = "Taken on \(formattedDate)"
+            
+            author.text = photo.author
         }
     }
     
